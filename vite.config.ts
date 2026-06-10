899a7e5f222c8b56ebd5af234580c75152c97eb3
LingCent <125841624+LingCent@users.noreply.github.com>
2026-06-10 12:32:48 +0800

    chore: configure base path and update dummy keys
    Set base URL for deployment to GitHub Pages and simplify placeholder environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 加上这一行，替换为你的实际仓库名！
+      base: '/QR-Order-System/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || 'dummy_key_for_portfolio'),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || 'dummy_key_for_portfolio')
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || 'dummy_key'),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || 'dummy_key')
       },
       resolve: {
         alias: {
